898ca82fda4661ec7ca390d90311677dfbc6040f
Deepak <dk8400779@gmail.com>
2022-08-25 20:45:08 +0530

    Initial Commit

--- src/com/company/Trees/TreeNode.java
+++ src/com/company/Trees/TreeNode.java
@@ -4,9 +4,11 @@ public class TreeNode {
      int val;
      TreeNode left;
      TreeNode right;
+     TreeNode parent;
     public TreeNode(int val){
         this.val = val;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 }
