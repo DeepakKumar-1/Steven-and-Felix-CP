9e02d582b36222fd0bf987a1f94bcf6e1f054f84
Deepak <dk8400779@gmail.com>
2022-08-27 14:44:39 +0530

    Initial Commit

--- src/com/company/SegmentTree/SegmentTree.java
+++ src/com/company/SegmentTree/SegmentTree.java
@@ -1,5 +1,7 @@
 package com.company.SegmentTree;
 
+import com.company.Trees.Tree;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +23,42 @@ public class SegmentTree {
         segmentTree[index] = segmentTree[(2*index+1)] + segmentTree[(2*index+2)];
     }
 
+
+    public static void updateTree(int []arr, int []segmentTree, int start, int end, int TreeIndex, int updateIndex, int val){
+        if(start == end){
+            arr[updateIndex] = val;
+            segmentTree[TreeIndex] = val;
+            return;
+        }
+
+        int mid = start + (end - start)/2;
+        if(updateIndex > mid){
+            // go towards right subtree
+            updateTree(arr, segmentTree, mid+1, end, 2*TreeIndex  + 2, updateIndex, val);
+        } else{
+            // go towards left subtree
+            updateTree(arr, segmentTree, start, mid, 2*TreeIndex  + 1, updateIndex, val);
+        }
+
+        segmentTree[TreeIndex] = segmentTree[(2*TreeIndex+1)] + segmentTree[(2*TreeIndex+2)];
+    }
+
+    public static int sum(int []segmentTree, int start, int end, int treeIndex, int left, int right){
+        // case 1 : Completely Outside the Given range
+        if(start > right || end < left){
+            return 0;
+        }
+        // case 2 : Completely Inside the Given range
+        if(start >= left && end <= right){
+            return segmentTree[treeIndex];
+        }
+        // case 3 : Partially  Inside and Partially Outside the Given range
+        int mid = start + (end - start)/2;
+        int ans1 = sum(segmentTree, start, mid, 2*treeIndex+1, left, right);
+        int ans2 = sum(segmentTree, mid+1, end, 2*treeIndex+2, left, right);
+        return ans1+ans2;
+    }
+
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
@@ -36,6 +74,15 @@ public class SegmentTree {
         int [] segmentTree = new int[max_size];
         createSegmentTree(arr, segmentTree, 0, arr.length-1, 0);
         System.out.println(Arrays.toString(segmentTree));
+
+        updateTree(arr, segmentTree, 0, arr.length-1, 0, 2, 10);
+        System.out.println(Arrays.toString(segmentTree));
+
+
+        // Query
+        int ans = sum(segmentTree, 0, arr.length-1, 0, 2, 4);
+        System.out.println(ans);
+
     }
 
 
